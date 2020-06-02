dffa06f334600a3b9aaeb37dfbfa9ab53107293d
SeongJin <jsj0718tjdwl@naver.com>
2020-06-02 22:30:39 +0900

    Making Login Server Response

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const cors = require('cors');
+
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Fuck");
 });
 
-app.pos;
+app.post("/loginPost",(req,res)=>{
+  console.log(req.body);
+
+  res.set('Access-Control-Allow-Origin','*');
+  res.status(200).send({
+    "msg" : "Your Request is Successful"
+  });
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
